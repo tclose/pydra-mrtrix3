92fea0c9e1ee1c1dd7bc61ae4d916674a28b53c1
Xihe Xie <axiezai@gmail.com>
2021-02-18 12:29:34 -0500

    fixed AND saved solved conflict

--- pydra/tasks/mrtrix3/__init__.py
+++ pydra/tasks/mrtrix3/__init__.py
@@ -1,10 +1,6 @@
 """
 >>> import pydra
-<<<<<<< HEAD
->>> import pydra.tasks.MRtrix3
-=======
 >>> import pydra.tasks.mrtrix3
->>>>>>> 011f7c13c16e25bcaeb0d9224e490925d45f1550
 """
 from ._version import get_versions
 
