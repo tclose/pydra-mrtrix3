737587e81022c7d0b123c2a39e86be5142107fe9
Tom Close <tom.g.close@gmail.com>
2022-09-27 08:00:21 +1000

    fixed up argstr of out_file

--- pydra/tasks/mrtrix3/utils.py
+++ pydra/tasks/mrtrix3/utils.py
@@ -25,8 +25,10 @@ MRConvertInputSpec = SpecInfo(
         (
             "out_file",
             attr.ib(
-                type=File,
+                type=ty.Union[Path, str],
                 metadata={
+                    "position": -1,
+                    "argstr": "",
                     "help_string": "output image",
                     "output_file_template": "{in_file}_converted",
                 },
@@ -132,6 +134,10 @@ class MRConvert(ShellCommandTask):
     """
     Example
     ------
+
+    Convert NIfTI file with FSL-style gradient encoding files to
+    MRtrix Image Format with MRtrix-style gradient encoding files
+
     >>> task = MRConvert()
     >>> task.inputs.in_file = "test_dwi.nii.gz"
     >>> task.inputs.grad_fsl = ["test.bvec", "test.bval"]
@@ -139,6 +145,25 @@ class MRConvert(ShellCommandTask):
     >>> task.inputs.out_file = "test.mif"
     >>> task.cmdline
     'mrconvert test_dwi.nii.gz -fslgrad test.bvec test.bval -export_grad_mrtrix test.b test.mif'
+
+    Select the first volume from a diffusion-weighted dataset
+
+    >>> task = MRConvert()
+    >>> task.inputs.in_file = "test_dwi.nii.gz"
+    >>> task.inputs.out_file = "test_vol.nii.gz"
+    >>> task.inputs.coord = [3, 0]
+    >>> task.cmdline
+    'mrconvert test_dwi.nii.gz -coord 3 0 test_vol.nii.gz'
+
+    Extend a 3D image to 4D by adding a singular dimension
+
+    >>> task = MRConvert()
+    >>> task.inputs.in_file = "test.nii.gz"
+    >>> task.inputs.out_file = "test_set.nii.gz"
+    >>> task.inputs.axes = [0, 1, 2, -1]
+    >>> task.cmdline
+    'mrconvert test.nii.gz -axes 0,1,2,-1 test_set.nii.gz'
+
     """
 
     input_spec = MRConvertInputSpec
