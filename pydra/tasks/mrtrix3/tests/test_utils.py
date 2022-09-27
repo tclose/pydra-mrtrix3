56f51b2349066a1a73c9beb5525f2f16de007fa3
Tom Close <tom.g.close@gmail.com>
2022-09-28 08:04:46 +1000

    added new unittest to test default and explicit out_files

--- pydra/tasks/mrtrix3/tests/test_utils.py
+++ pydra/tasks/mrtrix3/tests/test_utils.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pytest
 from pydra.tasks.mrtrix3.utils import MRConvert
 from medimages4tests.dicom.mri.dwi.siemens.skyra.syngo_d13c import sample_image
@@ -8,10 +9,19 @@ def dicom_dataset():
     return sample_image()
 
 
-def test_mrconvert(dicom_dataset):
+def test_mrconvert_default_out_file(dicom_dataset):
+
+    task = MRConvert(in_file=dicom_dataset, coord=(3, 0))
+
+    result = task()
+
+    assert Path(result.output.out_file).exists()
+
+
+def test_mrconvert_explicit_out_file(dicom_dataset):
 
     task = MRConvert(in_file=dicom_dataset, out_file="test.nii.gz")
 
     result = task()
 
-    assert result.output.out_file.exists()
+    assert Path(result.output.out_file).exists()
